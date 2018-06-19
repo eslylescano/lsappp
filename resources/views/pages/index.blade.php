e80d73549596a3173db998647d88908aae9cfe4d
eslylescano <eslylescano@gmail.com>
2018-06-19 12:52:14 +0100

    Update index.blade.php

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
     <div class="jumbotron text-center">
-        <h1>Welcome to Laravel</h1>
+        <h1>Welcome to Esly Page</h1>
         <p>This is laravel application from Esly Lescano</p>
         <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a>
     <a class="btn btn-primary btn-lg" href="/register" role="button">Register</a></p>
